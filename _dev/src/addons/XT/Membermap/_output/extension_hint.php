bc6a2c9be26f0725b64f803abf4e593761ea2a64
McAtze <info@it-maku.com>
2024-04-09 17:51:04 +0200

    Create _dev directory

---
+++ _dev/src/addons/XT/Membermap/_output/extension_hint.php
@@ -0,0 +1,37 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace XT\Membermap\XF\Admin\Controller
+{
+	class XFCP_User extends \XF\Admin\Controller\User {}
+	class XFCP_UserGroup extends \XF\Admin\Controller\UserGroup {}
+}
+
+namespace XT\Membermap\XF\Entity
+{
+	class XFCP_User extends \XF\Entity\User {}
+	class XFCP_UserProfile extends \XF\Entity\UserProfile {}
+}
+
+namespace XT\Membermap\XF\Job
+{
+	class XFCP_UserAction extends \XF\Job\UserAction {}
+}
+
+namespace XT\Membermap\XF\Pub\Controller
+{
+	class XFCP_Account extends \XF\Pub\Controller\Account {}
+}
+
+namespace XT\Membermap\XF\Repository
+{
+	class XFCP_UserGroup extends \XF\Repository\UserGroup {}
+	class XFCP_UserProfile extends \XF\Repository\UserProfile {}
+}
+
+namespace XT\Membermap\XF\Searcher
+{
+	class XFCP_User extends \XF\Searcher\User {}
+}
